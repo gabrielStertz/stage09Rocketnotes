38a664f807f94ad06227132a1c0b24d0e1e01d1e
Gabriel <gabrielstertz2012@gmail.com>
2022-10-17 12:44:05 -0300

    initial commit

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,10 @@
+import { Container } from './style';
+export function Details(){
+  
+  return(
+    <Container>
+      <h1>Hello, world!</h1>
+      <span>Gabriel Stertz</span>
+    </Container>
+  );
+};
